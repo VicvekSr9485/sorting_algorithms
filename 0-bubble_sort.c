29f5c65754a48235e097a5aa109031b64bf14357
VicvekSr <Olamideoso65@gmail.com>
2022-07-16 20:50:28 +0000

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,21 @@
 #include "sort.h"
 
 /**
+* swap_func - a function to swap array elements
+* @a: the array to swap
+* @b: the array to be swapped
+*/
+
+void swap_func(int *a, int *b)
+{
+    int tmp;
+
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+/**
 * bubble_sort - function that sorts an array of integers in ascending order
 * using the Bubble sort algorithm
 *
