81b3c5ea57dafcfd104a925c6ba3a647253c7fd8
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-07-16 20:59:34 +0000

    modified bubble sort file

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -28,19 +28,24 @@ void swap_func(int *a, int *b)
 void bubble_sort(int *array, size_t size)
 {
     size_t i, j, flag;
-    for (i = 0; i < size - 1; i++)
-     {
-        flag = 0;
-        for (j = 0; j < size - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                swap_func(&array[j], &array[j + 1]);
-                print_array(array, size);
-                flag = 1;
-            }
-        }
-        if (flag == 0)
-            break;
-     }
+
+    if (size >= 2)
+    {
+    	for (i = 0; i < size - 1; i++)
+		{
+			flag = 0;
+			for (j = 0; j < size - 1 - i; j++)
+			{
+				if (array[j] > array[j + 1])
+				{
+					swap(&array[j], &array[j + 1]);
+					print_array(array, size);
+					flag = 1;
+				}
+			}
+			if (flag == 0)
+				break;
+		}
+    }
+    
 }
