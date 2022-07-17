759589a836887c8e11e6e27b2fc16406afacf6d4
ayobamy <ayobamyahmed@gmail.com>
2022-07-16 17:27:19 -0700

    sorting algorithm for selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -17,32 +17,32 @@ void swap_func(int *a, int *b)
 
 /**
  * selection_sort - sorts an array of elements
- * @array - array of elements
+ * @array: array of elements
  * @size: Number of elements in @array
  *
  */
 
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, j_min;
+	size_t i, j, j_min;
 
-    if (size >= 2)
-    {
-    	for (i = 0; i < size - 1; i++)
-    	{
-    	    j_min = i;
-    		for (j = i + 1; j < size; j++)
-    		{
-    			if (array[j] < array[j_min])
-    			{
-    			    j_min = j;
-    			}
-    		}
-    		if (j_min != i)
-            {
-            swap_func(&array[i], &array[j_min]);
-            print_array(array, size);
-            }
-    	}
-    }
+	if (size >= 2)
+	{
+		for (i = 0; i < size - 1; i++)
+		{
+			j_min = i;
+			for (j = i + 1; j < size; j++)
+			{
+				if (array[j] < array[j_min])
+				{
+					j_min = j;
+				}
+			}
+			if (j_min != i)
+			{
+				swap_func(&array[i], &array[j_min]);
+				print_array(array, size);
+			}
+		}
+	}
 }
