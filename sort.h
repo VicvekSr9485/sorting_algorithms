2231c892602d28f6ba6d486781b23a8a454d3841
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-07-18 21:46:40 +0000

    added prototypes to header file

--- sort.h
+++ sort.h
@@ -25,5 +25,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+int partition(int *array, int start, int end, size_t size);
+void quick_recursive(int *array, int start, int end, size_t size);
 
 #endif /*__SORT__H*/
