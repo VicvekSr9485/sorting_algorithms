9c7b1945c1a3fb5cdf6f099cfa9cbfc52df04c8c
VicvekSr <Olamideoso65@gmail.com>
2022-07-19 21:29:45 +0000

    add prototype for insertion sort

--- sort.h
+++ sort.h
@@ -27,5 +27,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 int partition(int *array, int start, int end, size_t size);
 void quick_recursive(int *array, int start, int end, size_t size);
+void insertion_sort_list(listint_t **list);
 
 #endif /*__SORT__H */
