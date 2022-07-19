6e5edb904b9593c810601807b6e2bafbe6d367b8
VicvekSr <Olamideoso65@gmail.com>
2022-07-19 21:39:45 +0000

    add prototype for shell sort to header file

--- sort.h
+++ sort.h
@@ -28,5 +28,6 @@ void quick_sort(int *array, size_t size);
 int partition(int *array, int start, int end, size_t size);
 void quick_recursive(int *array, int start, int end, size_t size);
 void insertion_sort_list(listint_t **list);
+void shell_sort(int *array, size_t size);
 
 #endif /*__SORT__H */
