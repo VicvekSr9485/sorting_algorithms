fed63af714f713c3b565bff86b7faccf3edb971b
VicvekSr <Olamideoso65@gmail.com>
2022-07-19 21:49:13 +0000

    add prototype for Cocktail shaker sort

--- sort.h
+++ sort.h
@@ -29,5 +29,6 @@ int partition(int *array, int start, int end, size_t size);
 void quick_recursive(int *array, int start, int end, size_t size);
 void insertion_sort_list(listint_t **list);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 
 #endif /*__SORT__H */
