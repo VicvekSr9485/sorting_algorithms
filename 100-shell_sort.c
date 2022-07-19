c8d4990c2522f512722b0663faad2f74cc5cb163
VicvekSr <Olamideoso65@gmail.com>
2022-07-19 21:53:41 +0000

    add shell sort algorithm

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,27 +1,46 @@
 #include "sort.h"
 
 /**
- * shell_sort - Shell sort - Knuth Sequence
+ * shell_sort - shell sorting algorithm using insertion sort on subarrays
  * @array: array to sort
  * @size: size of array
  */
 void shell_sort(int *array, size_t size)
 {
-	int gap = 1, i, j, tmp;
+	size_t gap, i, j, k;
+	int tmp;
 
-	while (gap < ((int)(size)))
-		gap = (3 * gap) + 1;
-	for (gap = (gap - 1) / 3; gap > 0; gap = (gap - 1) / 3)
+	for (gap = 1; gap < size; gap = 3 * gap + 1)
+		;
+	for (gap /= 3; gap > 0; gap /= 3)
 	{
-		for (i = gap; i < (int)size; i += 1)
+		for (i = 0; i < gap; i++)
 		{
-			tmp = array[i];
-
-			for (j = i; j >= gap && array[j - gap] > tmp; j -= gap)
-				array[j] = array[j - gap];
-
-			array[j] = tmp;
+			j = i;
+			while (j < size - gap)
+			{
+				if (array[j] > array[j + gap])
+				{
+					tmp = array[j];
+					array[j] = array[j + gap];
+					array[j + gap] = tmp;
+					for (k = j; k >= gap; k -= gap)
+					{
+					if (array[k] < array[k - gap])
+					{
+					tmp = array[k];
+					array[k] = array[k - gap];
+					array[k - gap] = tmp;
+					}
+					else
+					break;
+					}
+				}
+				else
+					j += gap;
+			}
 		}
 		print_array(array, size);
+
 	}
 }
